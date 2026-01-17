c5b6e69ff8fb83017cf037ad4f6e8b6d9e1b6097
Arti05-Ambre <ambrearti62@gmail.com>
2026-01-17 23:14:14 +0530

    Update short data type giveing commit.java

--- short data type.java
+++ short data type.java
@@ -3,7 +3,7 @@ class Codeshef{
         short regionOneIntensity = 12000;
         short regionTwoIntensity = -5000;
         byte regionThreeIntensity = 0;
-        
+        //some data types
         System.out.println("Region One Intensity: " +   regionOneIntensity);
         System.out.println("Region Two Intensity: " +   regionTwoIntensity);
         System.out.println("Region Three Intensity: " +  regionThreeIntensity);
