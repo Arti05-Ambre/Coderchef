42fb6cb12433155806f5b9baba1feac4541e8c66
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-10 21:06:53 +0530

    Fix formatting in wallet balance update

--- -=.java
+++ -=.java
@@ -5,7 +5,7 @@ class Codechef {
         // Deducting expense using -= operator
         walletBalance -= 200;
  
-        // Printing the updated balance
+        // Printing the updated balance 
         System.out.println("Updated Wallet Balance: " + walletBalance);
     }
 } 
